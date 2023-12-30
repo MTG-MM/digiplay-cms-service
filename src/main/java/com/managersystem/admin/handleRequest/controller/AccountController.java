c06cb6ac910391f2f5a24dd681aa6c8516fe9689
lambro25102001@gmail.com <lambro25102001@gmail.com>
2023-12-30 15:55:58 +0700

    fix cors

--- src/main/java/com/managersystem/admin/handleRequest/controller/AccountController.java
+++ src/main/java/com/managersystem/admin/handleRequest/controller/AccountController.java
@@ -4,13 +4,11 @@ import com.managersystem.admin.handleRequest.controller.dto.*;
 import com.managersystem.admin.server.service.AccountService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("api/mos/cms/auth")
+@CrossOrigin("*")
 public class AccountController {
 
   @Autowired

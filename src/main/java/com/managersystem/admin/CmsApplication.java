6c2fc5d5f2de221cc86db66d7911b7450e4782f8
lambro25102001@gmail.com <lambro25102001@gmail.com>
2024-02-10 14:16:45 +0700

    test

--- src/main/java/com/managersystem/admin/CmsApplication.java
+++ src/main/java/com/managersystem/admin/CmsApplication.java
@@ -2,12 +2,14 @@ package com.managersystem.admin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @SpringBootApplication
 @EnableWebSecurity
 @EnableScheduling
+@EnableJpaRepositories
 public class CmsApplication {
 
 	public static void main(String[] args) {

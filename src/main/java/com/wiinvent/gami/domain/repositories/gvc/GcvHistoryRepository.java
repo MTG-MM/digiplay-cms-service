eceb5d53dd24e953c16b3178bf44b5bd2c459e55
lam <lambro2510@gmail.com>
2024-04-01 09:05:11 +0700

    update

--- src/main/java/com/wiinvent/gami/domain/repositories/gvc/GcvHistoryRepository.java
+++ src/main/java/com/wiinvent/gami/domain/repositories/gvc/GcvHistoryRepository.java
@@ -1,9 +1,9 @@
 package com.wiinvent.gami.domain.repositories.gvc;
 
-import com.wiinvent.gami.domain.entities.gvc.GcvHistory;
+import com.wiinvent.gami.domain.entities.gvc.GvcHistory;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface GcvHistoryRepository extends JpaRepository<GcvHistory, String> {
+public interface GcvHistoryRepository extends JpaRepository<GvcHistory, String> {
 }

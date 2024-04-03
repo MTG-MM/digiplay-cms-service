e4ce666c86bda8daa22f2849a34fd3a28d18fed5
dattp6501 <truongphucdat6501@gmail.com>
2024-04-03 10:10:17 +0700

    validation create, update payment method

--- src/main/java/com/wiinvent/gami/domain/service/PaymentMethodService.java
+++ src/main/java/com/wiinvent/gami/domain/service/PaymentMethodService.java
@@ -4,8 +4,12 @@ import com.wiinvent.gami.domain.dto.payment.PaymentMethodCreateDto;
 import com.wiinvent.gami.domain.dto.payment.PaymentMethodUpdateDto;
 import com.wiinvent.gami.domain.entities.payment.PaymentMethod;
 import com.wiinvent.gami.domain.entities.type.Status;
+import com.wiinvent.gami.domain.exception.BadRequestException;
 import com.wiinvent.gami.domain.response.payment.PaymentMethodResponse;
+import com.wiinvent.gami.domain.utils.Constant;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Component;
@@ -13,9 +17,12 @@ import org.springframework.transaction.annotation.Isolation;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Objects;
+
 @Component
 @Log4j2
 public class PaymentMethodService extends BaseService{
+  @Autowired @Lazy private PaymentMethodService self;
   public Page<PaymentMethodResponse> findAll(Pageable pageable){
     Page<PaymentMethod> payments = paymentMethodStorage.findAll(pageable);
     return modelMapper.toPagePaymentMethodResponse(payments);
@@ -27,23 +34,42 @@ public class PaymentMethodService extends BaseService{
     return modelMapper.toPaymentMethodResponse(paymentMethod);
   }
 
-  @Transactional(isolation = Isolation.SERIALIZABLE, propagation = Propagation.REQUIRED)
   public boolean createPaymentMethod(PaymentMethodCreateDto dto){
     if(dto.getStatus()==null) dto.setStatus(Status.ACTIVE);
 
+    /*
+    * neu payment method type da ton tai
+    * */
+    if(Objects.nonNull(paymentMethodStorage.findPaymentMethodByPaymentMethodType(dto.getPaymentMethodType())))
+      throw new BadRequestException(Constant.PAYMENT_METHOD_EXISTS);
+
     PaymentMethod paymentMethod = modelMapper.toPaymentMethod(dto);
 
-    paymentMethodStorage.save(paymentMethod);
+    self.savePaymentMethod(paymentMethod);
+    return true;
+  }
+
+  public boolean updatePaymentMethod(PaymentMethodUpdateDto dto){
+    PaymentMethod paymentMethod = paymentMethodStorage.findById(dto.getId());
+
+    /*
+    * neu thay doi payment method type:
+    * neu da ton tai => loi
+    * */
+    if(
+        !dto.getPaymentMethodType().equals(paymentMethod.getPaymentMethodType())
+        && Objects.nonNull(paymentMethodStorage.findPaymentMethodByPaymentMethodType(dto.getPaymentMethodType()))
+    ) throw new BadRequestException(Constant.PAYMENT_METHOD_EXISTS);
+
+    paymentMethod.from(dto);
+
+    self.savePaymentMethod(paymentMethod);
     return true;
   }
 
   @Transactional(isolation = Isolation.SERIALIZABLE, propagation = Propagation.REQUIRED)
-  public boolean updatePaymentMethod(PaymentMethodUpdateDto dto){
-    PaymentMethod paymentMethod = paymentMethodStorage.findById(dto.getId());
-    paymentMethod.from(dto);
-
+  public void savePaymentMethod(PaymentMethod paymentMethod){
     paymentMethodStorage.save(paymentMethod);
-    return true;
   }
 
   @Transactional(isolation = Isolation.SERIALIZABLE, propagation = Propagation.REQUIRED)
--- src/main/java/com/wiinvent/gami/domain/stores/payment/PaymentMethodStorage.java
+++ src/main/java/com/wiinvent/gami/domain/stores/payment/PaymentMethodStorage.java
@@ -1,12 +1,23 @@
 package com.wiinvent.gami.domain.stores.payment;
 
+import com.wiinvent.gami.domain.entities.game.GameType;
 import com.wiinvent.gami.domain.entities.payment.PaymentMethod;
+import com.wiinvent.gami.domain.entities.type.PaymentMethodType;
 import com.wiinvent.gami.domain.exception.base.ResourceNotFoundException;
 import com.wiinvent.gami.domain.stores.BaseStorage;
 import com.wiinvent.gami.domain.utils.Constant;
+import jakarta.persistence.criteria.CriteriaBuilder;
+import jakarta.persistence.criteria.CriteriaQuery;
+import jakarta.persistence.criteria.Predicate;
+import jakarta.persistence.criteria.Root;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
 
 @Component
 public class PaymentMethodStorage extends BaseStorage {
@@ -23,5 +34,18 @@ public class PaymentMethodStorage extends BaseStorage {
 
   public void deleteById(Integer id){
     paymentMethodRepository.deleteById(id);
+  }
+
+  public PaymentMethod findPaymentMethodByPaymentMethodType(PaymentMethodType paymentMethodType){
+    return paymentMethodRepository.findOne(specification(null, paymentMethodType)).orElseThrow(()->new ResourceNotFoundException(Constant.PAYMENT_METHOD_NOT_FOUND));
+  }
+
+  private Specification<PaymentMethod> specification(Integer id, PaymentMethodType paymentMethodType) {
+    return (Root<PaymentMethod> root, CriteriaQuery<?> query, CriteriaBuilder criteriaBuilder) -> {
+      List<Predicate> predicates = new ArrayList<>();
+      if(Objects.nonNull(id)) predicates.add(criteriaBuilder.equal(root.get("id"), id));
+      if(Objects.nonNull(paymentMethodType)) predicates.add(criteriaBuilder.equal(root.get("paymentMethodType"), paymentMethodType));
+      return criteriaBuilder.and(predicates.toArray(new Predicate[0]));
+    };
   }
 }

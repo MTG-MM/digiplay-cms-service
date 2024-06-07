c06698bb9c414ab06fc5d13d490de0008b7dc572
tuannaptit2001 <zrosez14@gmail.com>
2024-06-07 13:37:27 +0700

    update

--- src/main/java/com/wiinvent/gami/domain/dto/InternalTaskRequestDto.java
+++ src/main/java/com/wiinvent/gami/domain/dto/InternalTaskRequestDto.java
@@ -1,17 +1,13 @@
 package com.wiinvent.gami.domain.dto;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
 import com.wiinvent.gami.domain.entities.type.RequestGamiType;
 import lombok.Data;
 
-import java.time.LocalDate;
 import java.util.UUID;
 @Data
 public class InternalTaskRequestDto {
   private UUID userId;
   private Integer itemId;
   private Integer score;
-  @JsonFormat(pattern = "yyyy-MM-dd")
-  private LocalDate date;
   private RequestGamiType requestType;
 }
